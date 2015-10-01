24bd28aff4209f9b2870b98ed721bf7f0f48c48f
Don J <kahdojay@gmail.com>
2015-10-01 17:03:48 -0400

    Add taskView.

---
+++ src/components/taskView.js
@@ -0,0 +1,32 @@
+const React = require('react-native');
+import { BackBtn } from '../utilities/navigation';
+
+const {
+  StyleSheet,
+  View,
+  Text,
+} = React;
+
+class TaskView extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <BackBtn 
+          navigator={this.props.navigator}
+        />
+        <Text>Recipe TaskView</Text>
+      </View>
+    );
+  }
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 30
+  }
+});
+
+
+module.exports = TaskView;
