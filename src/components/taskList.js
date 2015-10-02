3246769d5792a63a29f67370f4e200675e4539a7
Don J <kahdojay@gmail.com>
2015-10-02 16:13:48 -0400

    Divide tasklist into sections

--- src/components/taskList.js
+++ src/components/taskList.js
@@ -4,16 +4,36 @@ import TaskListItem from './taskListItem';
 let {
   View,
   PropTypes,
+  Text
 } = React;
 
 export default class TaskList extends React.Component {
   render() {
-    let taskKeys = Object.keys(this.props.tasks);
+    const tasks = this.props.tasks
+    let taskKeys = Object.keys(tasks);
+    let tasksCompleted = []
+    let tasksIncomplete = [];
+    taskKeys.forEach(function(taskKey){
+      if (tasks[taskKey].completed) {
+        tasksCompleted.push(tasks[taskKey])
+      } else {
+        tasksIncomplete.push(tasks[taskKey])
+      }
+    })
+    console.log('taskList', tasksCompleted, tasksIncomplete)
     return (
       <View>
-        {taskKeys.map((taskKey, index) => {
-          let task = this.props.tasks[taskKey]
-          return <TaskListItem 
+        {tasksCompleted.map((task) => {
+          return <TaskListItem
+            name={task.name}
+            completed={task.completed}
+            taskId={task.id}
+            navigator={this.props.navigator}
+            onPress={() => this.props.onTaskClick(task.id)} />
+        })}
+        <Text>Divider</Text>
+        {tasksIncomplete.map((task) => {
+          return <TaskListItem
             name={task.name}
             completed={task.completed}
             taskId={task.id}
