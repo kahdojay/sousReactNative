362e5a36acbe17a2906fee7615b84fa59d399f6b
license2e <ilya@shindyapin.com>
2015-10-04 21:26:24 -0400

    Refactor the reducers
    Signed-off-by: license2e <ilya@shindyapin.com>

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import task from './task'
+import team from './team'
 import station from './station'
 import session from './session'
 
 const reducers = combineReducers(Object.assign(
   {},
   task,
+  team,
   station,
   session
 ));
