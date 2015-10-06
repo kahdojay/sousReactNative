53ac6c04d5e5642c6e60fe8974316aa75b3de8ca
Don J <kahdojay@gmail.com>
2015-10-06 15:11:48 -0400

    Fix Hide/Show completed items filter logic.

--- src/components/taskList.js
+++ src/components/taskList.js
@@ -1,10 +1,14 @@
 import React from 'react-native';
 import TaskListItem from './taskListItem';
-import Divider from './divider';
+import {
+  greyText,
+  taskCompletedBackgroundColor
+} from '../utilities/colors';
 
 let {
   View,
   PropTypes,
+  StyleSheet,
   Text,
   TouchableHighlight,
 } = React;
@@ -48,13 +52,39 @@ export default class TaskList extends React.Component {
         <TouchableHighlight   
           onPress={this.handlePress.bind(this)}    
         >    
-          <Text>{tasksCompleted.count} Complete Tasks</Text>    
+          <View style={styles.container}>
+            <View style={styles.roundedCorners}>
+              <Text style={styles.text}>{tasksCompleted.length} Completed Items</Text>
+            </View>
+          </View>
         </TouchableHighlight>
         {this.state.showCompleted ? tasksCompleted : <View />}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    padding: 5,
+  },
+  roundedCorners: {
+    backgroundColor: taskCompletedBackgroundColor,
+    width: 150,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    backgroundColor: taskCompletedBackgroundColor,
+    fontWeight: 'bold',
+    color: greyText,
+    paddingTop: 5,
+    paddingBottom: 3,
+    width: 140,
+  },
+})
 
 TaskList.propTypes = {
   // onTaskClick: PropTypes.func.isRequired,
