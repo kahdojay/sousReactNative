c292374933e9f18c129369662c8eb2d373537872
Harry Moreno <morenoh149@gmail.com>
2015-10-06 12:26:58 -0400

    Revert api endpoint to localhost

--- src/resources/apiConfig.js
+++ src/resources/apiConfig.js
@@ -1,6 +1,6 @@
 
-// export const API_ENDPOINT = 'http://localhost:3000/api/1';
-export const API_ENDPOINT = 'http://sousapp.local:3000/api/1';
+export const API_ENDPOINT = 'http://localhost:3000/api/1';
+// export const API_ENDPOINT = 'http://sousapp.local:3000/api/1';
 
 export const endpoints = {
   ENDPOINT_SESSION: '/sessions',
