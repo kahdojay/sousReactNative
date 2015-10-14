4257bbcfde95b14771d9238dc533850bd674d0c6
Harry Moreno <morenoh149@gmail.com>
2015-10-14 13:45:53 -0400

    Add react-native-addressbook and display contacts as rows.

--- src/components/inviteView.js
+++ src/components/inviteView.js
@@ -1,6 +1,9 @@
 import React from 'react-native';
+import AddressBook from 'react-native-addressbook';
+import CheckBox from 'react-native-checkbox'
 
 const {
+  ScrollView,
   View,
   Text,
   PropTypes,
@@ -10,18 +13,60 @@ const {
 class InviteView extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      contacts: [],
+    }
+  }
+
+  componentDidMount() {
+    AddressBook.getContacts( (err, contacts) => {
+      if (err && err.type === 'permissionDenied') {
+        // TODO: show error to user
+        console.log('error fetching contacts')
+      }
+      else {
+        console.log('contacts:', contacts)
+        this.setState({ contacts: contacts });
+      }
+    })
+  }
+
+  // accepts an array of numbers (cleaned/unclean tbd)
+  sendSMS(numbers) {
+
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>test</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        {
+          this.state.contacts.map(function(contact, idx) {
+            return (
+              <View
+                style={styles.contactRow}
+                key={idx}
+              >
+                <Text>{contact.firstName} {contact.lastName}</Text>
+                <CheckBox
+                  label=''
+                  onChange={() => console.log('tapped')}
+                  checked={false}
+                />
+              </View>
+            );
+          })
+        }
+      </ScrollView>
     );
   }
 }
 
 let styles = StyleSheet.create({
+  contactRow: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  }
 })
 
 InviteView.propTypes = {
