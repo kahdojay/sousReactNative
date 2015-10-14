5a8343d7096d311e27dba78cb6f21aa25a803c65
tgoldenberg <thomasagoldenberg@gmail.com>
2015-10-14 17:06:51 -0400

    begin styling for UserInfo component

--- src/components/userInfo.js
+++ src/components/userInfo.js
@@ -8,6 +8,8 @@ let {
   StyleSheet,
   Text,
   View,
+  ScrollView,
+  TextInput,
   TouchableHighlight
 } = React;
 
@@ -15,11 +17,189 @@ let {
 class UserInfo extends React.Component{
   render(){
     return (
-      <View><Text>HI</Text></View>
+      <ScrollView
+        style={styles.scrollView}
+        keyboardShouldPersistTaps={false}
+        automaticallyAdjustContentInsets={false}
+      >
+        <View style={styles.wrapper}>
+          <View style={styles.userInfoContainer}>
+            <View style={styles.userProfile}>
+              <View style={styles.infoField}>
+                <Text style={styles.inputName}>
+                  Great! Looks like this is your first time here.
+                </Text>
+              </View>
+            </View>
+            <View style={styles.userProfile}>
+              <View style={styles.infoField}>
+                <Text style={styles.inputName}>
+                  {"What's your name?"}
+                </Text>
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.userInfoContainer}>
+
+            <View style={styles.userProfile}>
+              <View style={styles.infoField}>
+                <Text style={styles.inputName}>First Name</Text>
+              </View>
+              <View style={styles.infoField}>
+                <Text style={styles.inputName}>Last Name</Text>
+              </View>
+
+            </View>
+
+            <View style={styles.deactivateContainer}>
+              <TouchableHighlight style={styles.deactivateButton}>
+                <Text style={styles.deactivateText}>Login</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     )
   }
 }
-
+let styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: '#f2f2f2',
+    flex: 1,
+  },
+  userInfoContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  userProfile: {
+    flex: 1,
+    backgroundColor: 'white',
+    margin: 10,
+  },
+  userPreferences: {
+    flex: 2,
+    margin: 10,
+    marginTop: 0,
+    backgroundColor: 'white',
+  },
+  infoField: {
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: '#fff',
+    height: 25,
+    paddingLeft: 5,
+    flexDirection: 'row',
+  },
+  inputName: {
+    flex: 1,
+    color: 'black',
+    fontWeight: 'bold',
+    fontFamily: 'OpenSans',
+    fontSize: 14,
+    letterSpacing: -.5,
+  },
+  inputInfo: {
+    fontFamily: 'OpenSans',
+    fontSize: 14,
+  },
+  deactivateContainer: {
+    flex: 1,
+    marginTop: 5,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  deactivateButton: {
+    backgroundColor: '#ddd',
+    padding: 10,
+    width: 100,
+    borderRadius: 7,
+  },
+  deactivateText:{
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#777',
+    textAlign: 'center',
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: mainBackgroundColor,
+    alignItems: 'stretch',
+  },
+  navbar: {
+    height: 70,
+    backgroundColor: '#1E00B1',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  navbarText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  sideNavbarText: {
+    color: 'white',
+    fontSize: 14,
+    textAlign: 'center',
+  },
+  icon: {
+    height: 30,
+    width: 30,
+    flex: 1,
+  },
+  logo: {
+    flex: 2.5,
+    alignItems: 'flex-end',
+    marginRight: 10
+  },
+  navbarPush: {
+    flex: 2,
+    alignItems: 'center'
+  },
+  userIcon: {
+    width: 90,
+    height: 90,
+    borderRadius: 45,
+    margin: 4,
+  },
+  wrapper: {
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    flexDirection: 'column',
+  },
+  phoneNumber: {
+    backgroundColor: '#1E00B1',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  phoneText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
+    padding: 5,
+  },
+  avatar: {
+    backgroundColor: '#f2f2f2',
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  nameInput: {
+    backgroundColor: 'white',
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+  },
+})
 
 
 
