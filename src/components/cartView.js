dd3dc99cc2788b6f84aca3efc2e74a072f9f6b1b
tgoldenberg <thomasagoldenberg@gmail.com>
2015-10-20 15:38:59 -0400

    add quantity to cart items

--- src/components/cartView.js
+++ src/components/cartView.js
@@ -6,6 +6,7 @@ const {
   View,
   Text,
   ScrollView,
+  StyleSheet,
 } = React;
 
 class CartView extends React.Component {
@@ -17,22 +18,24 @@ class CartView extends React.Component {
     console.log('CART PROPS', this.props.team);
     console.log('PURVEYORS', appState);
     return (
-      <ScrollView >
+      <ScrollView style={styles.scrollView}>
         { _.map(purveyors, (purveyor, idx) => {
           if (cart.orders[purveyor.id] != null) {
             let purveyorProducts = _.keys(cart.orders[purveyor.id].products);
             let productName = '';
             let productRows = purveyorProducts.map((purveyorProduct, index) => {
+              console.log('PRODUCT', cart.orders[purveyor.id].products[purveyorProduct])
+              let quantity = cart.orders[purveyor.id].products[purveyorProduct].quantity
               let appProduct = _.find(products, (product) => {
                 return product.id == purveyorProduct
               });
               productName = appProduct.name;
               if (productName)
-                return <Text key={index}>{productName}</Text>;
+                return <Text style={styles.productTitle} key={index}>{quantity} {productName}</Text>;
             });
             return (
-              <View>
-                <Text key={idx}>{purveyor.name}</Text>
+              <View style={styles.purveyorContainer}>
+                <Text style={styles.purveyorTitle} key={idx}>{purveyor.name}</Text>
                 {productRows}
               </View>
             );
@@ -43,5 +46,32 @@ class CartView extends React.Component {
   }
 }
 
+let styles = StyleSheet.create({
+  purveyorContainer: {
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  purveyorTitle: {
+    backgroundColor: Colors.lightBlue,
+    borderRadius: 2,
+    fontWeight: 'bold',
+    padding: 10,
+    color: 'white',
+    marginTop: 1,
+    fontFamily: 'OpenSans',
+    fontSize: 18,
+  },
+  productTitle: {
+    padding: 8,
+    fontFamily: 'OpenSans',
+    fontSize: 14,
+    backgroundColor: 'white',
+    marginTop: 1,
+  },
+  scrollView: {
+    backgroundColor: '#f2f2f2',
+    flex: 1,
+  },
+})
 
 export default CartView
