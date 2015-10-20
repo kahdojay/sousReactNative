1c615319084c5540e6b0fea639c205ca76ec9221
tgoldenberg <thomasagoldenberg@gmail.com>
2015-10-20 13:07:33 -0400

    add link to CartView

---
+++ src/components/cartView.js
@@ -0,0 +1,20 @@
+import { Icon } from 'react-native-icons';
+import React from 'react-native';
+import Colors from '../utilities/colors';
+
+const {
+  View,
+  Text,
+} = React;
+
+class CartView extends React.Component {
+
+  render() {
+    return (
+      <View><Text>HELLO THIS IS THE CART</Text></View>
+    );
+  }
+}
+
+
+export default CartView
