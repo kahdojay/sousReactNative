830cf839e1c5e976d5a0d0879467f679d1237704
tgoldenberg <thomasagoldenberg@gmail.com>
2015-10-21 14:54:07 -0400

    sync up MultiPickerIOS to state items and render

--- src/components/productCreate.js
+++ src/components/productCreate.js
@@ -13,57 +13,80 @@ const {
 } = React;
 
 class ProductCreate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      purveyor: this.props.purveyors.data[0].name,
+      category: _.find(this.props.appState.teams.data, (team) => {
+        return team.id === this.props.appState.session.teamId
+      }).categories[0].name,
+      amount: 1,
+      unit: 'bag',
+      purveyorSelected: false,
+      categorySelected: false,
+      amountSelected: false,
+      unitSelected: false,
+    }
+  }
   render() {
     let purveyors = _.pluck(this.props.purveyors.data, 'name');
     let currentTeamId = this.props.appState.session.teamId;
     let teams = this.props.appState.teams.data;
     let categories = _.pluck(_.find(teams, (team) => {return team.id == currentTeamId}).categories, 'name');
-    console.log('CURRENT TEAM', categories);
+    console.log('STATE', this.state);
     let units = ['bag', 'bunch', 'cs', 'dozen', 'ea', 'g', 'kg', 'lb', 'oz', 'pack', 'tub']
     console.log('PROPS', this.props);
     return (
       <ScrollView style={styles.scrollView}>
         <View style={styles.inputContainer}>
           <Text style={styles.inputTitle}>Name</Text>
-          <TextInput style={styles.inputField} placeholder='Name'/>
+          <TextInput style={styles.inputField} placeholder='Name'onChange={(e) => {
+              this.setState({name: e.nativeEvent.text})
+            }}/>
         </View>
         <View style={styles.emptySpace}></View>
         <View style={styles.inputContainer}>
           <Text style={styles.inputTitle}>Purveyor</Text>
-          <TextInput style={styles.inputField} placeholder='Purveyor'/>
+          <Text style={styles.inputField}> {this.state.purveyorSelected ? this.state.purveyor : 'Select Purveyor'}</Text>
         </View>
         <View style={styles.inputContainer}>
           <Text style={styles.inputTitle}>Category</Text>
-          <TextInput style={styles.inputField} placeholder='Category'/>
+          <Text style={styles.inputField}>{this.state.categorySelected ? this.state.category : 'Select Category'}</Text>
         </View>
-        <View style={styles.inputContainer}>
-          <Text style={styles.inputTitle}>Unit</Text>
-          <TextInput style={styles.inputField} placeholder='Unit'/>
-        </View>
+
         <View style={styles.inputContainer}>
           <Text style={styles.inputTitle}>Amount</Text>
-          <TextInput style={styles.inputField} placeholder='Amount'/>
+          <Text style={styles.inputField} >{this.state.amountSelected ? this.state.amount : 'Amount of'} {this.state.unitSelected ? this.state.unit : 'Units'}</Text>
         </View>
-        <MultiPickerIOS style={styles.picker} onChange={this._someOnChange}>
-          <Group selectedValue={'How'} >
+        <MultiPickerIOS style={styles.picker}>
+          <Group selectedValue={this.state.category} onChange={(e) => {
+              this.setState({category: e.newValue, categorySelected: true})
+            }}>
               {categories.map((category, idx) => {
                 return <Item value={category} key={idx} label={category} />;
               })}
           </Group>
-          <Group selectedValue='Uno'>
+          <Group selectedValue={this.state.purveyor} onChange={(e) => {
+              this.setState({purveyor: e.newValue, purveyorSelected: true})
+            }}>
               { purveyors.map((purveyor, idx) => {
                 return <Item value={purveyor} key={idx} label={purveyor} />
               })}
           </Group>
-
         </MultiPickerIOS>
         <MultiPickerIOS style={styles.picker}>
-          <Group selectedValue={1} >
+          <Group selectedValue={this.state.amount} onChange={(e) => {
+              console.log(e)
+              this.setState({amount: e.newValue, amountSelected: true});
+            }}>
             { _.range(1, 500).map((num) => {
               return <Item value={num} key={num} label={num.toString()} />
             })}
           </Group>
-          <Group selectedValue='Uno'>
+          <Group selectedValue={this.state.unit} onChange={(e) => {
+              this.setState({unit: e.newValue, unitSelected: true})
+            }}>
             { units.map((val, idx) => {
               return <Item value={val} key={idx} label={val} />
             })}
@@ -90,6 +113,10 @@ const styles = StyleSheet.create({
     fontSize: 14,
     padding: 8,
   },
+  picker: {
+    alignItems: 'stretch',
+    padding: 10,
+  },
   inputField: {
     flex: 1,
     backgroundColor: 'white',
@@ -99,6 +126,9 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     padding: 8,
     margin: 2,
+    fontFamily: 'OpenSans',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
   scrollView: {
     backgroundColor: '#f9f9f9',
