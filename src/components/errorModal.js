8b5ee08fb6a8da9c59cff11563c2b0ddb276e837
Harry Moreno <morenoh149@gmail.com>
2015-10-27 17:22:56 -0400

    Set Interval to attempt reconnect when offline, every 5 seconds.

--- src/components/errorModal.js
+++ src/components/errorModal.js
@@ -24,7 +24,6 @@ class ErrorModal extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    console.log('nextProps', nextProps)
     if (nextProps.errors.length > 0 || nextProps.connectionState.status === 'OFFLINE') {
       this.setState({
         errors: nextProps.errors,
