fed7e32ddb38335daa995f7e5347ccdf4130ca7c
Don J <kahdojay@gmail.com>
2015-10-27 21:58:54 -0400

    Finish renaming to Sous.

--- ios/SousTests/SousTests.m
+++ ios/SousTests/SousTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 240
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface sousmobileTests : XCTestCase
+@interface SousTests : XCTestCase
 
 @end
 
-@implementation sousmobileTests
+@implementation SousTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
