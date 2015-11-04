b953e5a87c3b77ab073a59ef40e979f59febe478
Harry Moreno <morenoh149@gmail.com>
2015-11-04 11:37:49 -0500

    In inviteView, vertically center contact names.

--- src/components/inviteView.js
+++ src/components/inviteView.js
@@ -95,6 +95,7 @@ let styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
     paddingRight: 10,
     paddingLeft: 10,
     borderBottomWidth: 1,
