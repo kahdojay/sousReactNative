e074b3ff78341edfda5b6756751eff2cf91ec6dd
license2e <ilya@shindyapin.com>
2015-11-09 19:20:08 -0500

    Convert the subscribe calls to use teamIds
    Signed-off-by: license2e <ilya@shindyapin.com>

--- src/components/categoryIndex.js
+++ src/components/categoryIndex.js
@@ -53,6 +53,14 @@ class CategoryIndex extends React.Component {
         >
           <Text style={styles.createButtonText}>Order by Purveyor</Text>
         </TouchableHighlight>
+        <TouchableHighlight
+          underlayColor='#eee'
+          onPress={this.props.onCreateProduct}
+          style={styles.createButton}
+        >
+          <Text style={styles.createButtonText}>Create New Product...</Text>
+        </TouchableHighlight>
+        <View style={styles.separator} />
         <ScrollView
           automaticallyAdjustContentInsets={false}
           keyboardShouldPersistTaps={false}
@@ -69,7 +77,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
   },
-  seperator: {
+  separator: {
     height: 5,
     borderBottomColor: '#bbb',
     borderBottomWidth: 1,
