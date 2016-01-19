4c4a4b2fd170892e74ada19c54f77b0a9903a5c1
Don J <kahdojay@gmail.com>
2016-01-18 19:23:15 -0500

    Login input checks state for phoneNumber before overriding from props.

--- src/components/signup.js
+++ src/components/signup.js
@@ -39,8 +39,13 @@ class Signup extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
+    let phoneNumber = this.state.phoneNumber
+    if(nextProps.session.phoneNumber !== ''){
+      phoneNumber = nextProps.session.phoneNumber
+    }
+
     let newState = {
-      phoneNumber: nextProps.session.phoneNumber,
+      phoneNumber: phoneNumber,
       smsSent: nextProps.session.smsSent,
     }
     if(this.state.submitting !== false){
