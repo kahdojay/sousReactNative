01169930bebec2c148a2b3c6c5175edbee6d8674
Don J <kahdojay@gmail.com>
2016-02-03 00:11:23 -0500

    Tweak order confirmation message based on cartPurveyors length.

--- src/components/cartView.js
+++ src/components/cartView.js
@@ -76,8 +76,9 @@ class CartView extends React.Component {
   }
 
   handleSubmitPress(cartPurveyors, singlePurveyor) {
+    console.log(cartPurveyors)
     if (this.props.connected === true && this.state.numberOfOrders > 0) {
-      let confirmationMessage = 'Send orders to all purveyors?'
+      let confirmationMessage = cartPurveyors.length > 1 ? `Send orders to ${cartPurveyors.length} purveyors?` : `Send order to ${cartPurveyors[0].name}?`
       let navigateToFeed = true
       if(singlePurveyor === true){
         confirmationMessage = `Send order to ${cartPurveyors[0].name}?`
